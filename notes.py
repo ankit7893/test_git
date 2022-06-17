9247dff7abb6c9b4f919ce5a7e7eca3ff9921b33
ankit <ankit_home@ankits-MacBook-Air.local>
2022-06-17 17:23:48 +0530

    second commit

---
+++ notes.py
@@ -0,0 +1,14 @@
+# first time  
+
+# git status 
+
+# git init 
+# git add . 
+# git commit -m "first commit"
+# git remote add origin https://github.com/ankit7893/test_git.git
+# git push origin 
+
+#########################################################
+
+# second time (changes )
+
